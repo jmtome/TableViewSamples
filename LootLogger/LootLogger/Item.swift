94775ecc5ce6a7acf82d6c65d9ba789c6acbf203
Juan Manuel Tome <jtome@fi.uba.ar>
2020-07-02 20:29:33 -0300

    Manual Codable
    Added a category item to implement manual codable with
    
    # encode(to:)
    
    and
    
    #init(from:)

--- LootLogger/LootLogger/Item.swift
+++ LootLogger/LootLogger/Item.swift
@@ -8,7 +8,75 @@
 
 import UIKit
 
+
+
 class Item: Equatable, Codable {
+    
+    enum Category {
+        case electronics
+        case clothing
+        case book
+        case other
+    }
+
+    var category = Category.other
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case valueInDollars
+        case serialNumber
+        case dateCreated
+        case isFavorite
+        case category
+        
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        try container.encode(valueInDollars, forKey: .valueInDollars)
+        try container.encode(serialNumber, forKey: .serialNumber)
+        try container.encode(dateCreated, forKey: .dateCreated)
+        try container.encode(isFavorite, forKey: .isFavorite)
+        
+        switch category {
+            case .electronics:
+                try container.encode("electronics", forKey: .category)
+            case .clothing:
+                try container.encode("clothing", forKey: .category)
+            case .book:
+                try container.encode("book", forKey: .category)
+            case .other:
+                try container.encode("other", forKey: .category)
+        }
+        
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.name = try container.decode(String.self, forKey: .name)
+        self.valueInDollars = try container.decode(Int.self, forKey: .valueInDollars)
+        self.serialNumber = try container.decode(String?.self, forKey: .serialNumber)
+        self.dateCreated = try container.decode(Date.self, forKey: .dateCreated)
+        self.isFavorite = try container.decode(Bool.self, forKey: .isFavorite)
+        
+        let categoryString = try container.decode(String.self, forKey: .category)
+        switch categoryString {
+        case "electronics":
+            self.category = .electronics
+        case "clothing":
+            self.category = .clothing
+        case "book":
+            self.category = .book
+        case "other":
+            self.category = .other
+        default:
+            self.category = .other
+        }
+    }
+    
+    
     static func == (lhs: Item, rhs: Item) -> Bool {
         return lhs.name == rhs.name && lhs.valueInDollars == rhs.valueInDollars && lhs.serialNumber == rhs.serialNumber && lhs.dateCreated == rhs.dateCreated
     }
